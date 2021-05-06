1ad1bddc523222c1eb618e51535d596c9a803f33
lorranasantos <lorrana.silva11020@gmail.com>
2021-05-06 16:56:58 -0300

    Primeiro Commit

---
+++ src/main/java/br/senai/sp/jandira/viagem/controller/DestinoController.java
@@ -0,0 +1,71 @@
+package br.senai.sp.jandira.viagem.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.senai.sp.jandira.viagem.dto.DestinosRecentesDTO;
+import br.senai.sp.jandira.viagem.model.Destino;
+import br.senai.sp.jandira.viagem.repository.DestinoRepository;
+
+@RestController
+@RequestMapping("/api")
+public class DestinoController {
+
+	@Autowired
+	private DestinoRepository destinoRepository;
+
+	@GetMapping("/destinos")
+	private List<DestinosRecentesDTO> getAll() {
+
+		List<Destino> destinos = new ArrayList<>();
+		List<DestinosRecentesDTO> destinosRecentes = new ArrayList<>();
+		destinos = destinoRepository.findAll();
+
+		for (Destino d : destinos) {
+			DestinosRecentesDTO dDTO = new DestinosRecentesDTO();
+			dDTO.setId(d.getId());
+			dDTO.setNome(d.getNome());
+			dDTO.setNomeCidade(d.getCidade().getNome());
+			dDTO.setSiglaEstado(d.getCidade().getEstado().getSigla());
+			dDTO.setValor(d.getValor());
+			destinosRecentes.add(dDTO);
+		}
+
+		return destinosRecentes;
+	}
+
+	@GetMapping("/destinos/{id}")
+	private Destino getDestino(@PathVariable Long id) {
+		return destinoRepository.findById(id).get();
+	}
+
+	@PostMapping("/destinos")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	private Destino saveDestino(@RequestBody Destino destino) {
+		return destinoRepository.save(destino);
+	}
+
+	@PutMapping("/destinos")
+	private Destino updateDestino(@RequestBody Destino destino) {
+		return destinoRepository.save(destino);
+	}
+
+	@DeleteMapping("/destinos/{id}")
+	private void deleteDestino(@PathVariable Long id) {
+		destinoRepository.deleteById(id);
+	}
+
+}
